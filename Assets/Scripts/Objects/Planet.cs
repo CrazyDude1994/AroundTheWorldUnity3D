52baad75b8a5c4a61ca5b86a14adf5c283bed056
Sergey Kartavtsev <crazyserega1994@mail.ru>
2014-12-04 19:07:23 +0200

    Added midpoint displacement algorithm. Created basic planet object

---
+++ Assets/Scripts/Objects/Planet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+
+    private float[] hills = new float[360];
+
+	void Start ()
+    {
+        generateHeightMap(hills, 0, 359, 360, 0.1f);
+	}
+
+    void generateHeightMap(float[] vector, int left, int right, int len, float r)
+    {
+        if (right - left < 2)
+        {
+            return;
+        }
+
+        float hl = vector[left];
+        float hr = vector[right];
+
+        float h = (hl + hr) / 2 + Random.Range(-r * len, r * len);
+        int index = Mathf.FloorToInt(left + (right - left) / 2);
+
+        vector[index] = h;
+
+        generateHeightMap(vector, left, index, len / 2, r);
+        generateHeightMap(vector, index, right, len / 2, r);
+    }
+	
+	void Update ()
+    {
+	
+	}
+}
